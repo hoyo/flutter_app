6442abee50f21d1b5eea941f189ece6f277ec74b
Keigo AOKI <hoyo1111@gmail.com>
2019-10-14 11:40:44 +0900

    fix format

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,19 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-const MaterialColor customSwatch = const MaterialColor(
+const MaterialColor customSwatch = MaterialColor(
   0xffff7d32,
   const <int, Color>{
-    50: const Color(0xffffefe6),
-    100: const Color(0xffffd8c2),
-    200: const Color(0xffffbe99),
-    300: const Color(0xffffa470),
-    400: const Color(0xffff9151),
-    500: const Color(0xffff7d32),
-    600: const Color(0xffff752d),
-    700: const Color(0xffff6a26),
-    800: const Color(0xffff601f),
-    900: const Color(0xffff4d13),
+    50: Color(0xffffefe6),
+    100: Color(0xffffd8c2),
+    200: Color(0xffffbe99),
+    300: Color(0xffffa470),
+    400: Color(0xffff9151),
+    500: Color(0xffff7d32),
+    600: Color(0xffff752d),
+    700: Color(0xffff6a26),
+    800: Color(0xffff601f),
+    900: Color(0xffff4d13),
   },
 );
 
@@ -25,20 +25,18 @@ class MyApp extends StatelessWidget {
   final String title = 'Flutter Demo';
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: title,
-        theme: ThemeData(
-          primarySwatch: customSwatch,
-          appBarTheme: AppBarTheme(
-            iconTheme: IconThemeData(color: Colors.white),
-            textTheme: Theme.of(context)
-                .textTheme
-                .apply(bodyColor: Colors.white, displayColor: Colors.white),
-          ),
+  Widget build(BuildContext context) => MaterialApp(
+      title: title,
+      theme: ThemeData(
+        primarySwatch: customSwatch,
+        appBarTheme: AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.white),
+          textTheme: Theme.of(context)
+              .textTheme
+              .apply(bodyColor: Colors.white, displayColor: Colors.white),
         ),
-        home: MyHomePage(title: title));
-  }
+      ),
+      home: MyHomePage(title: title));
 }
 
 class MyHomePage extends StatefulWidget {
@@ -79,17 +77,18 @@ class _MyHomePageState extends State<MyHomePage> {
     Widget scaffold;
     if (Platform.isIOS) {
       scaffold = CupertinoTabScaffold(
-          tabBar: CupertinoTabBar(items: _tabItems),
-          tabBuilder: (BuildContext context, int index) {
-            return CupertinoTabView(builder: (BuildContext context) {
-              return CupertinoPageScaffold(
-                navigationBar: new CupertinoNavigationBar(
-                  middle: Text(widget.title),
-                ),
-                child: _tabWidgets.elementAt(index),
-              );
-            });
+        tabBar: CupertinoTabBar(items: _tabItems),
+        tabBuilder: (BuildContext context, int index) {
+          return CupertinoTabView(builder: (BuildContext context) {
+            return CupertinoPageScaffold(
+              navigationBar: new CupertinoNavigationBar(
+                middle: Text(widget.title),
+              ),
+              child: _tabWidgets.elementAt(index),
+            );
           });
+        },
+      );
     } else {
       scaffold = Scaffold(
         appBar: AppBar(
@@ -104,9 +103,6 @@ class _MyHomePageState extends State<MyHomePage> {
           onTap: (int index) => setState(() => _selectedIndex = index),
         ),
         drawer: Drawer(
-          // Add a ListView to the drawer. This ensures the user can scroll
-          // through the options in the drawer if there isn't enough vertical
-          // space to fit everything.
           child: ListView(
             // Important: Remove any padding from the ListView.
             padding: EdgeInsets.zero,
@@ -125,7 +121,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       content: Text('この機能は実装されていません'),
                       actions: <Widget>[
                         FlatButton(
-                          child: const Text('OK'),
+                          child: Text('OK'),
                           onPressed: () => Navigator.pop(context),
                         ),
                       ],
